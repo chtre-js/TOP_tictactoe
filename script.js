39c79a1efdb440e86851d2ad050c58b17d2b7e25
Cherif <227361371+chtre-js@users.noreply.github.com>
2026-07-30 18:43:30 +0200

    feat(gameboard): add encapsulated board module
    * Implement IIFE pattern to privately encapsulate board state
    
    * Return shallow copy of board array in getter to prevent mutation
    
    * Add setter function with index and marker guards for cell updates
    
    * Implement reset function to clear board state to default null values

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ function createPlayer(name, marker) {
 
     return {getName, getMarker}
 }
+
+const gameboard = (function() {
+    const board = new Array(9);
+
+    function getBoard() {
+        return [...board];
+    }
+    
+    function setCell(index, marker) {
+        if (board[index] !== null) {
+            board.splice(index, 1, marker)
+        }
+    }
+
+    function resetBoard() {
+        board.fill(null);
+    }
+})();
